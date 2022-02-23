ad7beff994156551bace76b0daddbdddaf4dd579
luyuh <774964107@qq.com>
2022-02-23 10:28:27 +0800

    s22 passed

--- lang/python/s22.py
+++ lang/python/s22.py
@@ -7,25 +7,53 @@ class Solution:
     在「杨辉三角」中，每个数是它左上方和右上方的数的和。
     """
     def generateParenthesis(self, n: int) -> List[str]:
-        if n == 1:
-            return ["()"]
+        ln = n * 2
+        rs = []
+        for i in range(ln):
+            if len(rs) == 0:
+                rs.append('(')
+            else:
+                tmp = []
+                for r in rs:
+                    # add (
+                    l = r + '('
+                    if self.is_valid(l, n):
+                        tmp.append(l)
 
-        lf = ["("] * n
-        rg = [")"] * n
+                    # add )
+                    r = r + ')'
+                    if self.is_valid(r, n):
+                        tmp.append(r)
 
-        p = lf + rg
-        rs = ["".join(p)]
-
-        for i in range(n-1, 0, -1):
-            for j in range(n, 2*n-1):
-                p[i] = ')'
-                p[j] = '('
-                rs.append("".join(p))
-                p[i] = '('
-                p[j] = ')'
-
-        print(rs)
+                    rs = tmp
+        rs = list(filter(lambda x: self.is_complete(x), rs))
         return rs
+
+    def is_complete(self, st):
+        r = 0
+        for i in st:
+            if i == '(':
+                r += 1
+            elif i == ')':
+                r -= 1
+
+            if r < 0:
+                return False
+        return True if r == 0 else False
+
+    def is_valid(self, st, lv):
+        r = 0
+        for i in st:
+            if i == '(':
+                r += 1
+            elif i == ')':
+                r -= 1
+
+            if r < 0:
+                return False
+            if r > lv:
+                return False
+        return True
 
 
 
